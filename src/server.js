bcf729add272af763b91f785b8cf248842d2f98a
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-05-20 20:52:50 +0700

    set serve index at root path

--- src/server.js
+++ src/server.js
@@ -1,28 +1,26 @@
-const config = require("./config");
-const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const cors = require("cors");
+const config = require('./config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const cors = require('cors');
 const app = express();
-const FFA = require('./services/imageProcesser/compare')
+const FFA = require('./services/imageProcesser/compare');
 const port = process.env.PORT || 3000;
+const path = require('path');
 
-const db = require("./services/db");
-const applyRoutes = require("./routes");
+const db = require('./services/db');
+const applyRoutes = require('./routes');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(logger("dev"));
-
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 applyRoutes(app);
 
-app.get("/", (req, res) => {
-	FFA()
-	res.send("ROOT");
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-
-
 
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
