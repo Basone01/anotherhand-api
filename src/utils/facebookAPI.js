2ddc5a76cb730601336e5f5eaaf5356dac7d3b49
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-05-25 10:08:17 +0700

    pretified

--- src/utils/facebookAPI.js
+++ src/utils/facebookAPI.js
@@ -1,73 +1,70 @@
 const FormData = require('form-data');
 const fs = require('fs');
 const axios = require('axios');
-
 function getAttachmentId(filepathToUpload, token) {
-    const formData = new FormData();
-    formData.append("message", JSON.stringify({
-        "attachment": {
-            "type": "image",
-            "payload": {
-                "is_reusable": true
-            }
-        }
-    }))
-    const file = fs.createReadStream(filepathToUpload)
-    formData.append("filedata", file);
+	const formData = new FormData();
+	formData.append(
+		'message',
+		JSON.stringify({
+			attachment: {
+				type: 'image',
+				payload: {
+					is_reusable: true
+				}
+			}
+		})
+	);
+	const file = fs.createReadStream(filepathToUpload);
+	formData.append('filedata', file);
 
-    return axios
-        .create({
-            headers: formData.getHeaders()
-        })
-        .post(`https://graph.facebook.com/v2.6/me/message_attachments?access_token=${token}`, formData)
-        .then(response => response.data)
+	return axios
+		.create({
+			headers: formData.getHeaders()
+		})
+		.post(`https://graph.facebook.com/v2.6/me/message_attachments?access_token=${token}`, formData)
+		.then((response) => response.data);
 }
 
 async function sendImage(targetUserID, token, imagePath) {
-    try {
-        const { attachment_id } = await getAttachmentId(imagePath, token);
-        const { data } = await axios.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${token}`, {
-            "recipient": {
-                "id": targetUserID
-            },
-            "message": {
-                "attachment": {
-                    "type": "image",
-                    "payload": {
-                        attachment_id
-                    }
-                }
-
-            }
-        })
-        return data;
-    } catch (error) {
-        throw error;
-    }
-
+	try {
+		const { attachment_id } = await getAttachmentId(imagePath, token);
+		const { data } = await axios.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${token}`, {
+			recipient: {
+				id: targetUserID
+			},
+			message: {
+				attachment: {
+					type: 'image',
+					payload: {
+						attachment_id
+					}
+				}
+			}
+		});
+		return data;
+	} catch (error) {
+		throw error;
+	}
 }
 
 async function sendMessage(targetUserID, token, text) {
-    try {
-        const { data } = await axios.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${token}`, {
-            "recipient": {
-                "id": targetUserID
-            },
-            "message": {
-                text
-            }
-        })
-        return data;
-    } catch (error) {
-        throw error;
-    }
-
+	try {
+		const { data } = await axios.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${token}`, {
+			recipient: {
+				id: targetUserID
+			},
+			message: {
+				text
+			}
+		});
+		return data;
+	} catch (error) {
+		throw error;
+	}
 }
-
-
 
 module.exports = {
-    getAttachmentId,
-    sendImage,
-    sendMessage
-}
+	getAttachmentId,
+	sendImage,
+	sendMessage
+};
