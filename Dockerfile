cc89bc5602fce26c7ad66054b01464bbf216c5ca
Basone01 <basone01@hotmail.com>
2019-06-23 19:02:47 +0700

    revive

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,54 @@
-# FROM m03geek/opencv-node:alpine
-# # install sharp
-# RUN apk update && apk add -u --no-cache python make g++
-# RUN apk add vips-dev fftw-dev --update-cache --repository https://dl-3.alpinelinux.org/alpine/edge/testing/
+FROM justadudewhohacks/opencv-nodejs 
+
+RUN apt-get update  -y  && apt-get upgrade  -y   && apt-get install -y nodejs  python g++ cmake make git
+RUN apt install curl &&  curl -sL https://deb.nodesource.com/setup_8.x |  bash - &&  apt install nodejs
+COPY . /app/
+WORKDIR /app/
+RUN npm uninstall opencv4nodejs
+RUN npm install
+ENV NODE_PATH=/usr/lib/node_modules
+
+
+
+
+
+# # FROM m03geek/opencv-node:alpine
+# # # install sharp
+# # RUN apk update && apk add -u --no-cache python make g++
+# # RUN apk add vips-dev fftw-dev --update-cache --repository https://dl-3.alpinelinux.org/alpine/edge/testing/
+# # WORKDIR /app
+# # COPY ./package.json /app
+# # RUN npm i -g nodemon
+# # RUN npm i -g yarn
+# # RUN yarn
+# # COPY . /app
+# # CMD ["nodemon","-L", "index.js"]
+
+# FROM mhart/alpine-node:8.11.2 as nodebase
+
+# FROM m03geek/opencv:alpine
+# ENV OPENCV4NODEJS_DISABLE_AUTOBUILD=1
+
+# COPY --from=nodebase /usr/bin/node /usr/bin/
+# COPY --from=nodebase /usr/lib/node_modules /usr/lib/node_modules
+# COPY --from=nodebase /usr/include/* /usr/include/
+
+# RUN ln -s /usr/lib/node_modules/npm/bin/npm-cli.js /usr/bin/npm \
+#   && ln -s /usr/lib/node_modules/npm/bin/npx-cli.js /usr/bin/npx \
+#   && rm -rf /usr/local/lib && ln -s /usr/local/lib64 /usr/local/lib \
+#   && rm /usr/lib/node_modules/npm/*.md
+
+# # RUN apk update && apk add -u --no-cache python make g++ 
+
+# # RUN apk add vips-dev fftw-dev build-base --update-cache \
+# #     --repository https://alpine.global.ssl.fastly.net/alpine/edge/testing/ \
+# #     --repository https://alpine.global.ssl.fastly.net/alpine/edge/main \
+
+# RUN npm i -g nodemon \
+#   && npm i -g yarn
 # WORKDIR /app
 # COPY ./package.json /app
-# RUN npm i -g nodemon
-# RUN npm i -g yarn
-# RUN yarn
 # COPY . /app
+# RUN yarn
 # CMD ["nodemon","-L", "index.js"]
 
-FROM mhart/alpine-node:8.11.2 as nodebase
-
-FROM m03geek/opencv:alpine
-ENV OPENCV4NODEJS_DISABLE_AUTOBUILD=1
-
-COPY --from=nodebase /usr/bin/node /usr/bin/
-COPY --from=nodebase /usr/lib/node_modules /usr/lib/node_modules
-COPY --from=nodebase /usr/include/* /usr/include/
-
-RUN ln -s /usr/lib/node_modules/npm/bin/npm-cli.js /usr/bin/npm \
-  && ln -s /usr/lib/node_modules/npm/bin/npx-cli.js /usr/bin/npx \
-  && rm -rf /usr/local/lib && ln -s /usr/local/lib64 /usr/local/lib \
-  && rm /usr/lib/node_modules/npm/*.md
-
-RUN apk update && apk add -u --no-cache python make g++ \
-  && apk add vips-dev fftw-dev --update-cache --repository https://dl-3.alpinelinux.org/alpine/edge/testing/ \
-  && npm install -g npm@5.6.0 \
-  && npm i -g nodemon \
-  && npm i -g yarn
-WORKDIR /app
-COPY ./package.json /app
-COPY . /app
-RUN yarn
-CMD ["nodemon","-L", "index.js"]
--- src/services/facebook/index.js
+++ src/services/facebook/index.js
@@ -1,121 +1,133 @@
-const axios = require('axios').default;
-const createTemplateMessage = require('./templateMessage/fabebookGenericTemplate');
+const axios = require("axios").default;
+const createTemplateMessage = require("./templateMessage/fabebookGenericTemplate");
 
-function getAttachmentId({image_url, token}) {
-	return axios
-		.post(`https://graph.facebook.com/v2.6/me/message_attachments?access_token=${token}`, {
-			message: {
-				attachment: {
-					type: 'image',
-					payload: {
-						is_reusable: true,
-						url: image_url
-					}
-				}
-			}
-		})
-		.then((response) => {
-			return response.data;
-		});
+function getAttachmentId({ image_url, token }) {
+  return axios
+    .post(
+      `https://graph.facebook.com/v2.6/me/message_attachments?access_token=${token}`,
+      {
+        message: {
+          attachment: {
+            type: "image",
+            payload: {
+              is_reusable: true,
+              url: image_url
+            }
+          }
+        }
+      }
+    )
+    .then(response => {
+      return response.data;
+    });
 }
 
-async function sendProduct({customer_id, token, products}) {
-	try {
-		const { data } = await axios.post(
-			`https://graph.facebook.com/v2.6/me/messages?access_token=${token}`,
-			createTemplateMessage({
-				customer_id,
-				products
-			})
-		);
-		return data;
-	} catch (error) {
-		throw error;
-	}
+async function sendProduct({ customer_id, token, products }) {
+  try {
+    const { data } = await axios.post(
+      `https://graph.facebook.com/v2.6/me/messages?access_token=${token}`,
+      createTemplateMessage({
+        customer_id,
+        products
+      })
+    );
+    return data;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function sendImage({targetUserID, token, imagePath}) {
-	try {
-		const { attachment_id } = await getAttachmentId({image_url:imagePath, token});
-		const { data } = await axios.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${token}`, {
-			recipient: {
-				id: targetUserID
-			},
-			message: {
-				attachment: {
-					type: 'image',
-					payload: {
-						attachment_id
-					}
-				}
-			}
-		});
-		return data;
-	} catch (error) {
-		throw error;
-	}
+async function sendImage({ targetUserID, token, imagePath }) {
+  try {
+    const { attachment_id } = await getAttachmentId({
+      image_url: imagePath,
+      token
+    });
+    const { data } = await axios.post(
+      `https://graph.facebook.com/v2.6/me/messages?access_token=${token}`,
+      {
+        recipient: {
+          id: targetUserID
+        },
+        message: {
+          attachment: {
+            type: "image",
+            payload: {
+              attachment_id
+            }
+          }
+        }
+      }
+    );
+    return data;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function sendMessage({targetUserID, token, text}) {
-	try {
-		const { data } = await axios.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${token}`, {
-			recipient: {
-				id: targetUserID
-			},
-			message: {
-				text
-			}
-		});
-		return data;
-	} catch (error) {
-		throw error;
-	}
+async function sendMessage({ targetUserID, token, text }) {
+  try {
+    const { data } = await axios.post(
+      `https://graph.facebook.com/v2.6/me/messages?access_token=${token}`,
+      {
+        recipient: {
+          id: targetUserID
+        },
+        message: {
+          text
+        }
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(error.data);
+    throw error;
+  }
 }
 
-const getCustomerProfileFromPSID = async ({id, token}) => {
-	return axios.default
-		.get(`https://graph.facebook.com/v2.6/${id}?`, {
-			params: {
-				fields: 'first_name,last_name,profile_pic',
-				access_token: token
-			}
-		})
-		.then((res) => res.data);
+const getCustomerProfileFromPSID = async ({ id, token }) => {
+  return axios.default
+    .get(`https://graph.facebook.com/v2.6/${id}?`, {
+      params: {
+        fields: "first_name,last_name,profile_pic",
+        access_token: token
+      }
+    })
+    .then(res => res.data);
 };
-const getPageProfileFromPSID = async ({id, token}) => {
-	return axios.default
-		.get(`https://graph.facebook.com/v2.6/${id}?`, {
-			params: {
-				fields: 'name,picture{url}',
-				access_token: token
-			}
-		})
-		.then((res) => res.data);
+const getPageProfileFromPSID = async ({ id, token }) => {
+  return axios.default
+    .get(`https://graph.facebook.com/v2.6/${id}?`, {
+      params: {
+        fields: "name,picture{url}",
+        access_token: token
+      }
+    })
+    .then(res => res.data);
 };
-
 
 const addCustomerProfileToOrder = (order, token) => {
-	const orderWithCustomerDetails = new Promise(async (resolve, reject) => {
-		try {
-			const customer_profile = await getCustomerProfileFromPSID({
-				id: order.customer_id,
-				token: token
-			});
-			const orderWithCustomerProfile = { ...order, customer_profile };
-			resolve(orderWithCustomerProfile);
-		} catch (error) {
-			reject(error);
-		}
-	});
+  const orderWithCustomerDetails = new Promise(async (resolve, reject) => {
+    try {
+      const customer_profile = await getCustomerProfileFromPSID({
+        id: order.customer_id,
+        token: token
+      });
+      const orderWithCustomerProfile = { ...order, customer_profile };
+      resolve(orderWithCustomerProfile);
+    } catch (error) {
+      reject(error);
+    }
+  });
 
-	return orderWithCustomerDetails;
+  return orderWithCustomerDetails;
 };
 module.exports = {
-	getAttachmentId,
-	sendImage,
-	sendMessage,
-	sendProduct,
-	getCustomerProfileFromPSID,
-	getPageProfileFromPSID,
-	addCustomerProfileToOrder
+  getAttachmentId,
+  sendImage,
+  sendMessage,
+  sendProduct,
+  getCustomerProfileFromPSID,
+  getPageProfileFromPSID,
+  addCustomerProfileToOrder
 };
